a6518d0df11803bcd0a392f550cc142537804baf
brunobozz <brunobozz@gmail.com>
2023-02-23 18:21:54 -0300

    first commit

---
+++ src/services/pokeapi.service.js
@@ -0,0 +1,8 @@
+export default {
+  getData: async (endpoint) => {
+    const resp = await fetch("https://pokeapi.co/api/v2/" + endpoint);
+    return await resp.json();
+  },
+
+  postData: () => {},
+};
