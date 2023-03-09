994ed2b4d64e427e001c5e7057c9e2645cbc2ffd
bruno.bozzetti <bruno.bozzetti@dbserver.com.br>
2023-03-09 16:20:00 -0300

    created loading spinner component

---
+++ src/components/LoadingSpinner/LoadingSpinner.js
@@ -0,0 +1,9 @@
+import "./LoadingSpinner.scss";
+
+export default function LoadingSpinner() {
+  return (
+    <div className="loading-spinner">
+      <img src="https://i.pinimg.com/originals/05/8c/c1/058cc1913cf7d2bd93d6153ad22205f5.png" />
+    </div>
+  );
+}
